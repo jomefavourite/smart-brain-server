915062fbaf57ed7b4d01f924170afa3d3b59542b
jomefavourite <jfjomefavourite@gmail.com>
2021-04-25 23:36:21 +0100

    port
    Signed-off-by: jomefavourite <jfjomefavourite@gmail.com>

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const db = knex({
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 // app.get('/', (req, res) => {
 //   res.send(database.users);
@@ -40,8 +40,8 @@ app.put('/image', (req, res) => image.handleImage(req, res, db));
 
 app.post('/imageUrl', (req, res) => image.handleApiCall(req, res));
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
 
 /*
