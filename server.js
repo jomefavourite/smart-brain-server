54babc6421376c2a3d7d845610d5c972053d53fb
jomefavourite <jfjomefavourite@gmail.com>
2021-04-25 23:52:51 +0100

    upd
    Signed-off-by: jomefavourite <jfjomefavourite@gmail.com>

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.put('/image', (req, res) => image.handleImage(req, res, db));
 app.post('/imageUrl', (req, res) => image.handleApiCall(req, res));
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`);
+  console.log(`app is running on port ${process.env.PORT || 3000}`);
 });
 
 /*
